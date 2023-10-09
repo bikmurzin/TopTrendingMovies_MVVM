47e0470256686517c48aaf12b673d463e5fba9ad
Robert Bikmurzin <bikmurzinrm@gmail.com>
2023-10-09 15:19:46 +0400

    View Model Created

--- TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController.swift
+++ TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController.swift
@@ -12,24 +12,23 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // ViewModel:
+    var viewModel: MainViewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configView()
+    }
+    
+    func configView() {
         title = "Main View"
         view.backgroundColor = .red
+        
+        setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ TopTrendingMovies(MVVM)/ViewModel/MainViewModel.swift
@@ -0,0 +1,25 @@
+////   /*
+//
+//  Project: TopTrendingMovies(MVVM)
+//  File: MainViewModel.swift
+//  Created by: Robert Bikmurzin
+//  Date: 09.10.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
+
+class MainViewModel {
+    
+    func numberOfSections() -> Int {
+        1
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+    
+    
+}
