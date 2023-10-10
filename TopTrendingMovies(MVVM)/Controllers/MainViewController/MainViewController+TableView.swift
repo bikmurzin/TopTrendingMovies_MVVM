869e39dc66183ba59e86b1206a64d998ceead940
Robert Bikmurzin <bikmurzinrm@gmail.com>
2023-10-10 23:43:33 +0400

    Added and used class Observable

--- TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController+TableView.swift
+++ TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController+TableView.swift
@@ -27,6 +27,12 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
+    func reloadTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         viewModel.numberOfSections()
     }
@@ -37,8 +43,8 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = "\(indexPath.row)"
+        let movieData = self.cellDataSource[indexPath.row]
+        cell.textLabel?.text = self.viewModel.getMovieTitle(movieData)
         return cell
     }
 }
--- TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController.swift
+++ TopTrendingMovies(MVVM)/Controllers/MainViewController/MainViewController.swift
@@ -15,15 +15,22 @@ class MainViewController: UIViewController {
     
     // IBOutlets
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // ViewModel:
     var viewModel: MainViewModel = MainViewModel()
+    
+    // variables
+    var cellDataSource: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configView()
+        bindViewModel()
     }
+    
+    
     
     func configView() {
         title = "Main View"
@@ -32,7 +39,31 @@ class MainViewController: UIViewController {
         setupTableView()
     }
     
+    func bindViewModel() {
+        viewModel.isLoading.bind { [weak self] isLoading in
+            guard let self = self, let isLoading = isLoading else {
+                return
+            }
+            DispatchQueue.main.async {
+                if isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }
+        
+        viewModel.cellDataSource.bind { [weak self] movies in
+            guard let self = self, let movies = movies else {
+                return
+            }
+            self.cellDataSource = movies
+            self.reloadTableView()
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         viewModel.getData()
     }
 }
